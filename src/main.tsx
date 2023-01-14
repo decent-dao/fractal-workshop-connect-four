d8fa923e04fa9a18a7db768a37eb7052a88a64c7
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-01-14 01:46:29 -0500

    runs correctly

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import { Web3Provider } from './provider/web3/Web3Provider'
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '@decent-org/fractal-ui'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Web3Provider>
-      <App />
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
     </Web3Provider>
   </React.StrictMode>,
 )
