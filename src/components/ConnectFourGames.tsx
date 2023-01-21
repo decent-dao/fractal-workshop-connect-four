641cb47de043cbca3fb43eb66d147ce4d169b348
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-01-20 19:17:11 -0500

    cleans up all games screena nd adds link to readme

--- src/components/ConnectFourGames.tsx
+++ src/components/ConnectFourGames.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, SkeletonText, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+import { Button, Flex, Link, SkeletonText, Table, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react'
 import { constants } from 'ethers'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
@@ -10,7 +10,13 @@ import { GameBase } from '../provider/store/types'
 export function ConnectFourGames() {
   const { currentSeason: { gameIds } } = useStore()
   return (
-    <TableContainer>
+    <TableContainer maxW="6xl" mx="auto">
+      <Text textStyle="text-lg-mono-semibold" my={4}>
+        For Instructions on how to start a game and play see
+        <Link ml={2} color="blue.400" href='https://github.com/curiousity-labs/fractal-workshop-connect-four'>
+          README
+        </Link>
+      </Text>
       <Table variant='striped'>
         <Thead textStyle='text-base-mono-bold'>
           <Tr>
