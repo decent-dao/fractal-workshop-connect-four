4573f63497f14fd2fd80a4de2500fbe15426e454
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-01-24 09:23:06 -0500

    remove blue stand

--- src/components/board/SquareRow.tsx
+++ src/components/board/SquareRow.tsx
@@ -1,13 +1,11 @@
 import { Flex } from '@chakra-ui/react'
 import { ReactNode } from 'react'
-import { BOARD_PADDING } from '../../constants'
 
 export function SquareRow({ index, children }: { index: number; children: ReactNode }) {
   return (
     <Flex
-      borderX={index !== 0 ? BOARD_PADDING : undefined}
-      borderColor='blue.400-hover'
-    >
+      borderX={index !== 0 ? '1px solid' : undefined}
+      borderColor="gold.500">
       {children}
     </Flex>
   )
