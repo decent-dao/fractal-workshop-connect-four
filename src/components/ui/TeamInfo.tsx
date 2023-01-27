df23a0bf6e9445c96d1fa5ef8fe04b2882c77082
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-01-26 22:07:17 -0500

    fix turn conditional

--- src/components/ui/TeamInfo.tsx
+++ src/components/ui/TeamInfo.tsx
@@ -50,7 +50,7 @@ export function TeamInfo({ teamNum }: { teamNum: number }) {
           </Text>
           <Coin
             animation={
-              (isCurrentTurn && !isGameOver) || winningTeamNum === teamNum
+              isCurrentTurn || (!isGameOver && winningTeamNum === teamNum)
                 ? rotateAnimation
                 : 'none'
             }
