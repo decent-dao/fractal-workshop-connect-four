4ba1f8e3b86d18b47357c9cec51c0b5a5330c35e
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-01-31 09:51:47 -0500

    fix header on smaller screens

--- src/components/pages/ConnectFourGames.tsx
+++ src/components/pages/ConnectFourGames.tsx
@@ -51,12 +51,12 @@ export function ConnectFourGames() {
   }, [currentGame, dispatch])
   return (
     <Grid
-      gridTemplateAreas={{ 'min1000': smallScreen, 'min1920': largeScreen }}
+      gridTemplateAreas={{ 'min0': smallScreen, 'min1920': largeScreen }}
       templateColumns={{
-        'min1000': 'minmax(1fr, 72rem)',
+        'min0': 'repeat(1, 1fr)',
         'min1920': 'minmax(300px, 412px) 1fr',
       }}
-      templateRows={{ 'min1000': '5rem 10rem 1fr', 'min1920': 'calc(100vh)' }}
+      templateRows={{ 'min0': '5rem 1fr', 'min1920': 'calc(100vh)' }}
     >
       <GridItem area='brand' bg='black.900-semi-transparent'>
         <Branding />
